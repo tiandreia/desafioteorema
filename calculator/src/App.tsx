d3dd33f11676e929a31de991285c225061871e9b
tiandreia <tiandreiarocha@gmail.com>
2022-10-03 13:18:26 -0300

    setup inicial

---
+++ calculator/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      calculator
+    </div>
+  )
+}
+
+export default App
---
+++ calculator/src/components/Layout.tsx
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return (
+    <div>
+      <h1> Pythagorean Theorem Calculator</h1>
+    </div>
+  )
+}
+
+export default Layout

08846a822de6d8d572d6a7ff9249d4c910c3074b
tiandreia <tiandreiarocha@gmail.com>
2022-10-03 23:06:57 -0300

    Funções

--- calculator/src/components/Layout.tsx
+++ calculator/src/components/Layout.tsx
@@ -1,18 +1,58 @@
 import Triangle from '../assets/Triangle.png'
+import { useForm } from 'react-hook-form'
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
-const Layout = () => {
+// Formulário
+type PitagorasForm = {
+  catetoA: number
+  catetoB: number
+}
+
+const Layout: React.FC = () => {
+  // Validações dos campos do formulário
+  const validations = Yup.object().shape({
+    catetoA: Yup.number().required('Preencha o cateto A').moreThan(0),
+    catetoB: Yup.number().required('Preencha o cateto B').moreThan(0)
+  })
+
+  // Eventos
+  const { register, handleSubmit, reset } = useForm<PitagorasForm>({
+    resolver: yupResolver(validations)
+  })
+
+  // Função de cálculo da hipotenusa
+  const calcularHipotenusa = (form: PitagorasForm) => {
+    return Math.sqrt(Math.pow(form.catetoA, 2) + Math.pow(form.catetoB, 2))
+  }
+
+  // Função que captura evento de submissão do formulário
+  const onSubmit = (form: PitagorasForm) => {
+    //TODO: Atribuir resultado a uma varoável para exibi-lo em tela
+    console.log(calcularHipotenusa(form))
+  }
+
   return (
     <div>
       <h1> Pythagorean Theorem Calculator</h1>
       <img src={Triangle} alt="Triangle" />
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Cateto A</label>
+          <input {...register('catetoA')} type="number" />
+        </div>
+        <div>
+          <label>Cateto B</label>
+          <input {...register('catetoB')} type="number" />
+        </div>
+      </form>
+
       <div>
-        <input type="text" id="catA"/>
+        <button type="submit">Resultado</button>
       </div>
       <div>
-        <input type="text" id="catB"/>
-      </div>
-      <div>
-        <button id="buttom">Resultado</button>
+        <button onClick={() => reset()}>Limpar</button>
       </div>
     </div>
   )
